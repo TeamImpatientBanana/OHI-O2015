c71cb3a50d1729172508c8f352922e8dd749a87f
Joe Duncko <personal@joeduncko.com>
2015-11-14 15:53:44 -0500

    chat should work

--- routes/index.js
+++ routes/index.js
@@ -9,8 +9,8 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/chat', function(req, res, next) {
-    res.render('index', {
-        title: 'Express'
+    res.render('chat', {
+        title: 'Express | Chat'
     });
 });
 
