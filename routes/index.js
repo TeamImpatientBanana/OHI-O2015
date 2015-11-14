7e7e08140b72e9c03ee6b647eabf35caa2879a91
Joe Duncko <personal@joeduncko.com>
2015-11-14 14:23:20 -0500

    4 spaces

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', {
+        title: 'Express'
+    });
+});
+
+router.get('/chat', function(req, res, next) {
+    res.render('index', {
+        title: 'Express'
+    });
 });
 
 module.exports = router;
