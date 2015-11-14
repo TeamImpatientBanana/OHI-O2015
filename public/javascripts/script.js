0571b407e3c45e8310149f6a3010cf684b91ba2b
Joe Duncko <personal@joeduncko.com>
2015-11-14 17:47:02 -0500

    messages?

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,1 +1,10 @@
 var socket = io();
+
+$('form').submit(function(){
+  socket.emit('chat message', $('#chat-input').val());
+  $('#chat-input').val('');
+  return false;
+});
+socket.on('chat message', function(msg){
+  $('#messages').append($('<li>').text(msg));
+});
